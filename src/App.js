f4b0b560ece83cd21158740b4df61ea0d1a54175
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-17 11:33:49 +0900

    Feat: #7 Add input form component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { useCallback, useState } from 'react';
 import { LOCALE } from 'utils/constants';
 import LocaleTimer from 'components/Date/LocaleTimer';
+import InputForm from 'components/InputForm';
 import AscendingSort from 'components/Sort/AscendingSort';
 import DescendingSort from 'components/Sort/DescendingSort';
 
 function App() {
   const [date, setDate] = useState(new Date());
+  const [inputNumbers, setInputNumbers] = useState([]);
+  const [numbers, setNumbers] = useState([]);
 
   const handleDate = useCallback(() => {
     setDate(new Date());
   }, []);
 
+  const handleChange = (e) => {
+    // TO DO : 예외처리 추가
+    const numbers = e.target.value.split(',');
+    setInputNumbers(numbers);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNumbers(inputNumbers);
+    setInputNumbers([]);
+  };
+
   return (
     <div>
       <LocaleTimer date={date} locale={LOCALE.koKR} handleDate={handleDate} />
-      <AscendingSort />
-      <DescendingSort />
+      <InputForm
+        inputNumbers={inputNumbers}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <AscendingSort numbers={numbers} />
+      <DescendingSort numbers={numbers} />
       <LocaleTimer date={date} locale={LOCALE.enUS} />
     </div>
   );
