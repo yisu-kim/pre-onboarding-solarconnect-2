18295263db334047448416f63d6315a068a739b1
yisu-kim <yisu.kim.dev@outlook.com>
2021-09-17 19:32:30 +0900

    Refactor: Apply ts to utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,13 +1,13 @@
 import { TIMEZONE } from './constants';
 
-export const getLocaleDate = (date, locale) => {
+export const getLocaleDate = (date: Date, locale: string): string => {
   return date.toLocaleDateString(locale, {
     timeZone: TIMEZONE,
     dateStyle: 'full',
   });
 };
 
-export const getLocaleTime = (date, locale) => {
+export const getLocaleTime = (date: Date, locale: string): string => {
   return date.toLocaleTimeString(locale, {
     timeZone: TIMEZONE,
     timeStyle: 'medium',
